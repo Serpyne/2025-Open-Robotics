4122c7255a3ff4852d59935e92ad0bbfe76ca4e6
Serpyne <robbingchew@gmail.com>
2025-02-21 12:21:44 +0000

    PI

--- main.py
+++ main.py
@@ -10,24 +10,24 @@ motors = [
     Motor(address=0x1c)
 ]
 
-async def main(motors):
-    motors[0].set_speed_for(1.0, 3.0)
-    motors[0].set_speed_for(0.0, 1.0)
-    motors[0].set_speed_for(0.5, 2.0)
-    motors[1].set_speed_for(1.0, 0.1)
-    motors[0].set_speed_for(0.0, 0.1)
-    motors[1].set_speed_for(0.0, 0.1)
+#async def main(motors):
+#    motors[0].set_speed_for(1.0, 3.0)
+#    motors[0].set_speed_for(0.0, 1.0)
+#    motors[0].set_speed_for(0.5, 2.0)
+#    motors[1].set_speed_for(1.0, 0.1)
+#    motors[0].set_speed_for(0.0, 0.1)
+#    motors[1].set_speed_for(0.0, 0.1)
 
-def initialise_event_loop():
+def initialise_event_loop(main_func):
     loop = asyncio.get_event_loop()
     for motor in motors:
         loop.create_task(motor.event_loop())
-    loop.create_task(main(motors))
+    loop.create_task(main_func(motors))
     loop.run_forever()
 
-def mainloop():
+def mainloop(main_func):
     try:
-        initialise_event_loop()
+        initialise_event_loop(main_func)
     except KeyboardInterrupt:
         dribbler_motor.set_speed(0)
         for motor in motors:
--- uploads/robot.py
+++ uploads/robot.py
@@ -2,10 +2,11 @@ import sys
 import os
 parent_dir = "\\".join(os.path.dirname(__file__).split("\\")[:-1])
 sys.path.append(parent_dir)
-from main import *
+from main import mainloop
 
-def main(motors):
-    print(motors[0].i2c_address)
+async def main(motors):
+    for i in range(4):
+        motors[i].set_speed_for(1.0, 2)
 
 if __name__ == "__main__":
-    mainloop()
+    mainloop(main)
